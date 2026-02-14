b13e8fb84dbe3303a900e85050b7cbbb6601c168
scroll2learn <73377680+scroll2learn@users.noreply.github.com>
2026-02-14 08:48:13 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,12 @@
 provider "google" {
-  project = "<PROJECT_ID>"
+  project = "heroviredacademics"
   region  = "us-central1"
   zone    = "us-central1-a"
 }
 
 terraform {
   backend "gcs" {
-    bucket = "BACKEND_BUCKET_NAME"
+    bucket = "terraform-githubactions-integration-1302"
     prefix = "terraform/state"
   }
 } 
